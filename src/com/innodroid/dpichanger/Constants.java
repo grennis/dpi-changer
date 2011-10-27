0da93fb4b3b305a70c179a13b9ebb55c412d689d
Greg Ennis <admin@innodroid.com>
2011-10-26 20:21:18 -0400

    Added spinner to select predefined DPI values

--- src/com/innodroid/dpichanger/Constants.java
+++ src/com/innodroid/dpichanger/Constants.java
@@ -8,4 +8,20 @@ public class Constants {
 	public static final String CONFIG_FILE_NAME = "/system/build.prop";
 	public static final String BACKUP_FILE_NAME = new File(Environment.getExternalStorageDirectory(), "build.prop.bak").getAbsolutePath();
 	public static final String DPI_PREFIX = "ro.sf.lcd_density";
+	
+	public static final int[] DPI_VALUES = new int[] {
+		120,
+		160,
+		240,
+		320,
+		0
+	};
+	
+	public static final String[] DPI_NAMES = new String[] {
+		"LDPI (120)",
+		"MDPI (160)",
+		"HDPI (240)",
+		"XHDPI (320)",
+		"Custom"
+	};	
 }
--- src/com/innodroid/dpichanger/MainActivity.java
+++ src/com/innodroid/dpichanger/MainActivity.java
@@ -7,8 +7,13 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +27,11 @@ public class MainActivity extends Activity implements SetupTaskHandler, CommitTa
 	private static final int DIALOG_ERROR_READ = 102;
 	private static final int DIALOG_ERROR_COMMIT = 103;
 	private static final int DIALOG_DONE = 104;
-	private EditText mDpiText;
+	private ArrayAdapter<String> mDpiAdapter;
+	private EditText mCustomDpiText;
+	private TextView mCustomDpiWarning;
+	private ViewGroup mCustomDpiLayout;
+	private Spinner mDpiSpinner;
 	private Button mSaveButton;
 
 	@Override
@@ -30,8 +39,27 @@ public class MainActivity extends Activity implements SetupTaskHandler, CommitTa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        mDpiText = (EditText)findViewById(R.id.dpi_text);
+        mCustomDpiText = (EditText)findViewById(R.id.custom_dpi_text);
         mSaveButton = (Button)findViewById(R.id.save_button);
+        mCustomDpiLayout = (ViewGroup)findViewById(R.id.custom_dpi);
+        mDpiSpinner = (Spinner)findViewById(R.id.dpi_spinner);
+        mCustomDpiWarning = (TextView)findViewById(R.id.custom_warning);
+
+        mDpiAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Constants.DPI_NAMES);
+        mDpiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mDpiSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				int visibility = (arg2 == Constants.DPI_NAMES.length-1) ? View.VISIBLE : View.GONE;
+				mCustomDpiLayout.setVisibility(visibility);
+				mCustomDpiWarning.setVisibility(visibility);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}        	
+        });
+        mDpiSpinner.setAdapter(mDpiAdapter);
 
         ((TextView)findViewById(R.id.backup_location_text)).setText("Backup will be saved in " + Constants.BACKUP_FILE_NAME);
                 
@@ -83,14 +111,29 @@ public class MainActivity extends Activity implements SetupTaskHandler, CommitTa
 		if (dpi == 0) {
 			showDialog(DIALOG_ERROR_READ);
 		} else {
-			mDpiText.setText(Integer.toString(dpi));
-			mDpiText.setEnabled(true);
+			boolean found = false;
+			
+			for (int i=0; i<Constants.DPI_VALUES.length; i++) {
+				if (Constants.DPI_VALUES[i] == dpi) {
+					found = true;
+					mDpiSpinner.setSelection(i);
+					break;
+				}
+			}
+			
+			if (!found) {
+				mDpiSpinner.setSelection(Constants.DPI_VALUES.length-1);
+			}
+			
+			mCustomDpiText.setText(Integer.toString(dpi));
+			mCustomDpiText.setEnabled(true);
+			mDpiSpinner.setEnabled(true);
 			mSaveButton.setEnabled(true);
 		}
 	}
 	
 	private void commit() {
-		int dpi = parseAndValidateDpi();
+		int dpi = getSelectedDpi();
 
 		if (dpi == 0) {
 			Toast.makeText(this, "Enter a valid DPI value", Toast.LENGTH_SHORT).show();
@@ -100,10 +143,14 @@ public class MainActivity extends Activity implements SetupTaskHandler, CommitTa
 		new CommitTask(this, this, dpi).execute();
 	}
 
-	private int parseAndValidateDpi() {
+	private int getSelectedDpi() {
 		try {
-			int dpi = Integer.parseInt(mDpiText.getText().toString());
+			int selected = mDpiSpinner.getSelectedItemPosition();
+			int dpi = Constants.DPI_VALUES[selected];
 			
+			if (dpi == 0)
+				dpi = Integer.parseInt(mCustomDpiText.getText().toString()); 
+
 			// LDPI to XHDPI
 			if (dpi < 120 || dpi > 320) 
 				return 0;
